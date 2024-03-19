71832400f0d3da19c2a0dcbdc87e40b241ce48d9
Anthony M. Bonafide <anthony.bonafide@shipt.com>
2024-03-19 15:08:17 -0400

    Save point
    Signed-off-by: Anthony M. Bonafide <anthony.bonafide@shipt.com>

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,11 +1,15 @@
-use tguac::cache::Cache;
+use tguac::cache::{self, Cache};
+use tokio::io;
 use tokio::net::{TcpListener, TcpStream};
-#[tokio::main]
-async fn main() {
-    let mut listener = TcpListener::bind("127.0.0.1:8727").await.unwrap();
 
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8727").await.unwrap();
+    let database: Cache<String> = Cache::new();
     let (stream, _) = listener.accept().await.unwrap();
-    tokio::spawn(async move {});
+    tokio::spawn(async move { process(stream, database).await });
 }
 
-async fn process<T>(stream: TcpStream, db: Cache<T>) {}
+async fn process<T>(stream: TcpStream, db: Cache<T>) {
+    let (stream_writer, stream_reader) = io::split(stream);
+}
--- src/cache.rs
+++ src/cache.rs
@@ -2,4 +2,16 @@ use std::sync::Arc;
 
 use dashmap::DashMap;
 
-pub type Cache<T> = Arc<DashMap<String, T>>;
+pub struct Cache<T>(Arc<DashMap<String, T>>);
+
+impl<T> Cache<T> {
+    pub fn new() -> Self {
+        Cache(Arc::new(DashMap::<String, T>::new()))
+    }
+}
+
+impl<T> Default for Cache<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
